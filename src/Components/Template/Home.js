4174920d9a579398e02f9523e83cf5acbf1f260f
ArturoCruzT <artukok@gmail.com>
2019-10-05 23:50:46 -0500

    Home, Navbar

--- src/Components/Template/Home.js
+++ src/Components/Template/Home.js
@@ -2,6 +2,11 @@ import React from 'react';
 import {useState} from 'react';
 import * as langIndex from "../../lang";
 import Template from "./Template";
+import {esCelular} from "../../Helpers/GeneralHelper";
+import principal1 from "../../Img/principal-1.jpg";
+import principal2 from "../../Img/principal-2.jpg";
+import principal3 from "../../Img/principal-3.jpg";
+
 
 const Home = () => {
 	
@@ -25,7 +30,33 @@ const Home = () => {
 	return (
 		<Template lang={lang}
 				  lang_ok={lang_ok}>
-			hola
+			<div className="color-secudario vh-80">
+				<div className="container">
+					<div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+						<div className="carousel-inner">
+							<div className="carousel-item active">
+								<img className={`d-block w-100  ${esCelular()? "vh-25" : "vh-80"}`} src={principal1} alt="First slide"/>
+							</div>
+							<div className="carousel-item">
+								<img className={`d-block w-100  ${esCelular()? "vh-25" : "vh-80"}`} src={principal2} alt="Second slide"/>
+							</div>
+							<div className="carousel-item">
+								<img className={`d-block w-100  ${esCelular()? "vh-25" : "vh-80"}`} src={principal3} alt="Third slide"/>
+							</div>
+						</div>
+						<a className="carousel-control-prev" href="#carouselExampleControls" role="button"
+						   data-slide="prev">
+							<span className="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span className="sr-only">Previous</span>
+						</a>
+						<a className="carousel-control-next" href="#carouselExampleControls" role="button"
+						   data-slide="next">
+							<span className="carousel-control-next-icon" aria-hidden="true"></span>
+							<span className="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+			</div>
 		</Template>
 	);
 };
--- src/Components/Template/Navbar.js
+++ src/Components/Template/Navbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import { DiReact } from "react-icons/di";
 import { FaHome , FaUserAlt } from "react-icons/fa";
+import rex from "../../Img/rex.png";
 
 function Navbar({lang,  lang_ok}) {
 	return (
 		<div>
-			<nav className="navbar navbar-expand-lg navbar-light bg-dark">
-				<NavLink className="navbar-brand" to="#">{process.env.REACT_APP_APP_NAME}</NavLink>
-				<button className="navbar-toggler" type="button" data-toggle="collapse"
+			<nav className="navbar navbar-expand-lg navbar-light color-principal">
+				<NavLink className="navbar-brand text-white" to="#"><img style={{width:'30px' , height:'30px'}} src={rex}/>{process.env.REACT_APP_APP_NAME}</NavLink>
+				<button className="navbar-toggler text-white" type="button" data-toggle="collapse"
 						data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
 						aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
@@ -17,17 +18,17 @@ function Navbar({lang,  lang_ok}) {
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav mr-auto">
 						<li className="nav-item active">
-							<NavLink className="nav-link" to="/"><span> <FaHome/>  </span>Home <span className="sr-only">(current)</span></NavLink>
+							<NavLink className="nav-link text-white" to="/"><span> <FaHome/>  </span>Home <span className="sr-only">(current)</span></NavLink>
 						</li>
 						<li className="nav-item">
-							<NavLink className="nav-link" to="#">Link</NavLink>
+							<NavLink className="nav-link text-white" to="#">Link</NavLink>
 						</li>
 						<li className="nav-item dropdown">
-							<NavLink className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button"
+							<NavLink className="nav-link text-white dropdown-toggle" to="#" id="navbarDropdown" role="button"
 							   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 								Dropdown
 							</NavLink>
-							<div className="dropdown-menu" aria-labelledby="navbarDropdown">
+							<div className="dropdown-menu text-white" aria-labelledby="navbarDropdown">
 								<NavLink className="dropdown-item" to="#">Action</NavLink>
 								<NavLink className="dropdown-item" to="#">Another action</NavLink>
 								<div className="dropdown-divider"></div>
